dd45aa0bbcc242cfbe3e942668ba90699c51d051
Rovendoug <rovendoug@gmail.com>
2021-11-09 15:54:43 -0600

    Heroku Whitelist Update

--- index.js
+++ index.js
@@ -2,47 +2,48 @@ const exp = require("express");
 const app = exp();
 const fs = require("fs")
 const { exit } = require("process");
-const cors=require("cors");
+const cors = require("cors");
 require('dotenv').config()
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,
+const corsOptions = {
+    origin: '*',
+    credentials: true,
     optionSuccessStatus: 200,
     methods: "GET, PUT"
 }
+
 app.use(cors(corsOptions));
 app.use(exp.json());
 
 
 let csvToJson = require('convert-csv-to-json');
 
-    let rawdata = fs.readFileSync('config.json');
-    let config = JSON.parse(rawdata);
+let rawdata = fs.readFileSync('config.json');
+let config = JSON.parse(rawdata);
 
 
 if (!fs.existsSync('./whitelisted.csv')) {
     console.log("ERROR: No whitelisted.csv file has been found")
     exit(1)
 }
-if(!fs.existsSync('./whitelisted.json')){
+if (!fs.existsSync('./whitelisted.json')) {
     csvToJson.fieldDelimiter(',')
     csvToJson.generateJsonFileFromCsv("whitelisted.csv", "whitelisted.json")
     config.loaded = true;
     console.log("No whitelisted.json detected, converting csv to json")
 }
-if(!config.load_csv_onetime){
+if (!config.load_csv_onetime) {
     csvToJson.fieldDelimiter(',')
     csvToJson.generateJsonFileFromCsv("whitelisted.csv", "whitelisted.json")
     console.log("Json reloaded: config is set csv_run_onetime = false")
 }
-else{
+else {
     console.log("Skipping csv reload, config is set csv_run_onetime = true")
-    if(!config.loaded){
+    if (!config.loaded) {
         config.loaded = true
-        fs.writeFile('config.json', JSON.stringify(config), 'utf8', (err) =>{ if(err) console.log(err)});
+        fs.writeFile('config.json', JSON.stringify(config), 'utf8', (err) => { if (err) console.log(err) });
         csvToJson.fieldDelimiter(",")
-        csvToJson.generateJsonFileFromCsv("whitelisted.csv","whitelisted.json")
+        csvToJson.generateJsonFileFromCsv("whitelisted.csv", "whitelisted.json")
         console.log("Data reloaded due to config loaded = false")
     }
 }
@@ -50,7 +51,7 @@ else{
 
 
 
-app.get('/whitelisted', (req, res)=>{
+app.get('/whitelisted', (req, res) => {
     let rawdata = fs.readFileSync('whitelisted.json');
     let members = JSON.parse(rawdata);
     res.send(members)
@@ -61,14 +62,14 @@ app.get('/whitelisted/member/:address', (req, res, value) => {
     let rawdata = fs.readFileSync('whitelisted.json');
     let members = JSON.parse(rawdata);
     const user = members.find(c => c.member == req.params.address)
-    if(!user){
+    if (!user) {
         res.status(404).send("the member was not found");
     }
     res.send(user);
 })
 
 app.put('/whitelisted/update/:address/:secret', (req, res) => {
-    if(req.params.secret != process.env.SECRET_KEY){
+    if (req.params.secret != process.env.SECRET_KEY) {
         res.status(404).send("Invalid Auth Key")
         return
     }
@@ -77,21 +78,21 @@ app.put('/whitelisted/update/:address/:secret', (req, res) => {
     let rawdata = fs.readFileSync('whitelisted.json');
     let members = JSON.parse(rawdata);
     members.forEach((whitelisted_user, i) => {
-        if(whitelisted_user.member == req.params.address){
+        if (whitelisted_user.member == req.params.address) {
             result = whitelisted_user
             index = i;
         }
     });
-    if(result == {}){
+    if (result == {}) {
         res.status(404).send("User does not exist")
         return
     }
     result.reserve = req.body.reserve
     members[index] = result;
     const data = JSON.stringify(members);
-    fs.writeFile('whitelisted.json', data, 'utf8', (err) =>{ if(err) console.log(err)});
+    fs.writeFile('whitelisted.json', data, 'utf8', (err) => { if (err) console.log(err) });
     res.send(result)
 
 })
 const port = process.env.PORT || 3000
-app.listen(port, ()=>console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
